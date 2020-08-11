f8031e133ce21f1f5796f47d122d7df4f8868b88
Thoyt810 <66744198+Thoyt810@users.noreply.github.com>
2020-08-11 15:48:30 -0600

    Finalizing how to create heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Tomcat96!?",
-  database: "hamburger_db"
-});
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: process.env.password,
+//   database: process.env.database
+// });
+
+var connection = mysql.createConnection(process.env.JAWSDB_URL)
 
 connection.connect(function(err) {
   if (err) {
